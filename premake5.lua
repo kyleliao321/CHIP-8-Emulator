73f3491234b8396991d4f9dde37a6f0ea16f138d
Kyle <s8546937623@gmail.com>
2020-06-19 19:49:42 +0800

    Init premake5 scripte

---
+++ premake5.lua
@@ -0,0 +1,61 @@
+workspace "CHIP8"
+	architecture "x86_64"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CHIP8"
+	location "CHIP8"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "CHIP8/src/pch.cpp"
+
+	files
+	{
+		"%{prj.location}/src/**.h",
+		"%{prj.location}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/dependencies/SDL2/include"
+	}
+
+	libdirs
+	{
+		"%{prj.name}/dependencies/SDL2/lib/x64"
+	}
+
+	links
+	{
+		"SDL2.lib",
+		"SDL2main.lib"
+	}
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NDEBUG"
+		optimize "On"
+
+	filter "system:windows"
+		systemversion "latest"
+
+		postbuildcommands
+		{
+			("{COPY} %{prj.location}/dependencies/SDL2/lib/x64/SDL2.dll %{cfg.buildtarget.directory}")
+		}
